34387d95659c831ee94fe4d62f41d71352c83af6
lavp999 <lavp99@hotmail.com>
2022-12-02 20:44:04 +0000

    creadas las tablas, sin ENUM

--- src/models.py
+++ src/models.py
@@ -1,7 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
 
+
+
+db = SQLAlchemy()
+"""
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -16,4 +23,81 @@ class User(db.Model):
             "id": self.id,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+"""
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String(45), nullable=False)
+    last_name = db.Column(db.String(45), nullable=False)
+    email = db.Column(db.String(120), nullable=False)
+    password = db.Column(db.String(45), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "email": self.email,
+            "first_name": self.first_name,
+            "last_name": self.last_name
+        }
+
+
+
+class Planet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre
+        }
+
+
+class Character(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_planet = db.Column(db.Integer, ForeignKey('planet.id'), nullable=True)
+    nombre = db.Column(db.String(100), nullable=False)
+    planet = db.relationship(Planet)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "id_planet": self.id_planet,
+            "nombre": self.nombre,
+            "planet": self.planet
+        }
+
+class Favorit(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_user = db.Column(db.Integer, ForeignKey('user.id'))
+    tipo = db.Column(db.String(1), nullable=False)
+    id_character = db.Column(db.Integer, ForeignKey('character.id'), nullable=True)
+    id_planet = db.Column(db.Integer, ForeignKey('planet.id'), nullable=True)
+    user = db.relationship(User)
+    character = db.relationship(Character)
+    planet = db.relationship(Planet)
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "id_user": self.id_user,
+            "id_character": self.id_character,
+            "id_planet": self.id_planet,
+            "tipo": self.tipo,
+            "user": self.user,
+            "character": self.character,
+            "planet": self.planet
         }
